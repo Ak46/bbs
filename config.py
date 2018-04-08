229b010157fa83eff7fca9c15d57b11cd422a217
ak46 <1428018426@qq.com>
2018-04-08 13:31:51 +0800

    第4次提交 mysql的连接

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
 secret_key = 'secret'
 
-_db_path = 'db.sqlite'
-db_uri = 'sqlite:///{}'.format(_db_path)
+# _db_path = 'db.sqlite'
+# db_uri = 'sqlite:///{}'.format(_db_path)
+_db_path = ''
+db_uri = 'mysql+pymysql://{}'.format(_db_path)
